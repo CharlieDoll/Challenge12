1ba275b31b1455210f5ea360b03f5d39b457e24c
CharlieDoll <113851328+CharlieDoll@users.noreply.github.com>
2022-12-19 18:32:52 +0000

    seeds

---
+++ 02-Challenge/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department (id, name)
+VALUES  (2, "Sales"),
+        (3, "Engineering"),
+        (4, "Engineering"),
+        (5, "Finance"),
+        (6, "Finance"),
+        (7, "Legal"),
+        (8, "Legal");
+
+INSERT INTO employee (id, title, salary, department_id)
+VALUES  (2, "")
